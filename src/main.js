c4732b565b49b6c699e38f1ee1aead3706d33f77
Manish Murugan <76173590+Manishmrgn@users.noreply.github.com>
2024-05-16 15:37:27 +0530

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import Vue from "vue";
+import App from "./App.vue";
+import Typewriter from "typewriter-effect/dist/core";
+import GraphemeSplitter from "grapheme-splitter";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");
+
+const innerdemo = document.getElementById("inner-demo-2");
+
+const stringSplitter = (string) => {
+  const splitter = new GraphemeSplitter();
+  return splitter.splitGraphemes(string);
+};
+
+const typewriter = new Typewriter(innerdemo, {
+  loop: true,
+  delay: 45,
+  stringSplitter,
+});
+
+typewriter
+  .typeString("Hi, I'm Manish")
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("Nice to meet you!!")
+  .pauseFor(1000)
+  .deleteAll()
+  .start();
